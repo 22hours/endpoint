d582fb8dc06ab683db77e78d5907e8fc414a647f
김효빈 <52522662+sg05138@users.noreply.github.com>
2020-07-14 14:20:34 +0900

    [INITIAL] : 보일러플레이트 업데이트

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "reactstrap";
+
+const App = () => {
+    return (
+        <div>
+            <Button>안녕</Button>
+        </div>
+    );
+};
+export default App;
