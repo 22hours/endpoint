9d76fd23c04e20b5c9bf5119c23b91c315bf6090
김효빈 <52522662+sg05138@users.noreply.github.com>
2020-07-14 15:04:40 +0900

    [FEAT] : router 기능 추가

---
+++ src/AppRouter.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ScrollToTop from "module/ScrollToTop";
+import { Login, Home } from "page";
+
+const AppRouter = () => {
+    return (
+        <div className="AppRouter">
+            <Router>
+                <ScrollToTop>
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route path="/login" component={Login} />
+                        {/*api 전체 리스트 페이지*/}
+                        <Route path="/list/:apilist" />
+                        {/*특정api 조회 페이지*/}
+                        <Route path="/list/:apiname" />
+                        {/*api 추가페이지*/}
+                        <Route path="/list/add" />
+                    </Switch>
+                </ScrollToTop>
+            </Router>
+        </div>
+    );
+};
+
+export default AppRouter;
---
+++ src/page/Home.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+
+const Home = () => {
+    return <div className="Home">홈</div>;
+};
+
+export default Home;
