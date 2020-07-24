b2ec2478fe5933abd7531379ffd1987986c890aa
winterlood <king199777@gmail.com>
2020-07-24 19:23:43 +0900

    [FEAT] : 안쓰는 operation삭제, onclick기능 추가

--- src/module/NewEp/RequestHeaderItem/RequestHeaderItem.js
+++ src/module/NewEp/RequestHeaderItem/RequestHeaderItem.js
@@ -5,34 +5,9 @@ import { Button } from "reactstrap";
 import "./RequestHeaderItem.css";
 const RequestHeaderItem = (props) => {
     const { saveHeaderItem, removeHeaderItem } = useContext(NewEpStoreContext);
-
-    const InputChange = (type, value) => {
-        console.log(type);
-        console.log(value);
-        switch (type) {
-            case "_key":
-                saveHeaderItem(props.idx, {
-                    _key: value,
-                });
-                break;
-            case "value":
-                saveHeaderItem(props.idx, {
-                    value: value,
-                });
-                break;
-            case "descript":
-                saveHeaderItem(props.idx, {
-                    descript: value,
-                });
-                break;
-            default:
-                break;
-        }
-    };
-
     return (
         <>
-            <tr className="RequestHeaderItem">
+            <tr className="RequestHeaderItem" onClick={() => props.HandleEditItem(props.idx)}>
                 <td className="key">{props._key}</td>
                 <td className="value">{props.value}</td>
                 <td className="descript">{props.descript}</td>
