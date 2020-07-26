09f00b8201ca7da6250b6b87836cfd2c1152b7e3
김효빈 <52522662+sg05138@users.noreply.github.com>
2020-07-26 19:25:31 +0900

    [FEAT] : rest body inputbox 수정

--- src/module/NewEp/RequestModule/RequestModule.js
+++ src/module/NewEp/RequestModule/RequestModule.js
@@ -78,90 +78,13 @@ const RequestHeader = () => {
 const RequestBody = () => {
     const [bodytext, setBodyText] = useState("");
 
-    const setValue = (text) => {
-        setBodyText(text);
-        return bodytext.value;
-    };
-    const str_repeat = (str, n) => {
-        var out = "";
-        while (n--) out += str;
-        return out;
-    };
-    const isIndented = (line) => {
-        var regex = new RegExp("^(" + "\t" + "+)", "g"),
-            match = line.match(regex);
-        return (match && match[0].length / "\t".length) || 0;
-    };
-    const addIndent = (before, after, num) => {
-        if (!num) return;
-        bodytext._lastValue = setValue(before + str_repeat("\t", num) + after);
-        bodytext.selectionStart = bodytext.selectionEnd = before.length + "\t".length * num;
-    };
-    const removeIndent = (before, after) => {
-        var remove = before.slice(before.length - 1 - "\t".length, before.length - 1);
-        if (remove != "\t") {
-            return;
-        }
-
-        bodytext._lastValue = setValue(before.slice(0, -1 - "\t".length) + "}" + after);
-        bodytext.selectionStart = bodytext.selectionEnd = before.length - "\t".length;
-    };
-    const getPrevLine = (before) => {
-        var lines = bodytext.value.split(/\n/g),
-            line = before.trimRight().split(/\n/g).length - 1;
-        return lines[line] || "";
-    };
-
-    const Test1 = (e) => {
-        // var lastValue = bodytext?._lastValue,
-        //     change = bodytext?.length - lastValue?.length;
-        // if (!change) {
-        //     return;
-        // }
-        // var caret = bodytext.selectionStart,
-        //     added = (change > 0 && bodytext.substr(caret - change, change)) || "",
-        //     removed = (change < 0 && lastValue.substr(caret, -change)) || "";
-        // var code = e.keyCode;
-        // var value = bodytext,
-        //     before = value.substr(0, caret),
-        //     after = value.substr(caret),
-        //     lastChar = before.trim().slice(-1),
-        //     nextChar = after.substr(0, 1);
-        // // ENTER
-        // if (code === 13) {
-        //     // Immediately after a {
-        //     if (lastChar === "{") {
-        //         var prevLine = getPrevLine(before),
-        //             indents = isIndented(prevLine),
-        //             more = nextChar === "}" ? 0 : 1;
-        //         return addIndent(before, after, indents + more);
-        //     }
-        //     // After an indented line
-        //     var prevLine = getPrevLine(before),
-        //         indents = isIndented(prevLine),
-        //         more = nextChar === "}" ? -1 : 0;
-        //     if (indents + more > 0) {
-        //         addIndent(before, after, indents + more);
-        //     }
-        // } else if (added === "}") {
-        //     removeIndent(before, after);
-        // }
-    };
-
-    // const [startPos, setStartPos] = useState(0);
-    // const [endPos, setEndPos] = useState(0);
-
-    // const CursorLocation = (e) => {
-    //     setStartPos(bodytext.slice(0, e.target.selectionStart).length);
-    //     console.log(bodytext.slice(0, e.target.selectionStart).length);
-    // };
-
     // const Test = (e) => {
-    //     setStartPos(bodytext.slice(0, e.target.selectionStart).length);
-    //     console.log(bodytext.slice(0, e.target.selectionStart + 1));
+    //     var body = bodytext;
+    //     var startPos = body.slice(0, body.selectionStart).length;
+    //     console.log(body.slice(0, body.selectionStart).length + 1);
     //     e.preventDefault();
     //     if (e.keyCode === 9 || e.which === 9) {
-    //         setBodyText(bodytext.substring(0, startPos) + "\t" + bodytext.substring(startPos));
+    //         setBodyText(body.substring(0, startPos) + "\t" + body.substring(startPos));
     //     }
     // };
 
@@ -175,11 +98,9 @@ const RequestBody = () => {
                 onChange={({ target: { value } }) => {
                     setBodyText(value);
                 }}
-                placeholder="댓글을 입력하세요"
-                // onKeyDown={Test}
-                // onClick={Test}
-                onKeyUp={Test1}
-            />
+                placeholder='"name" : "hyobin",
+                "id" : "hbin12212",'
+            ></Input>
             <Button onClick={() => console.log(JSON.parse(bodytext))}>JSON</Button>
         </div>
     );
